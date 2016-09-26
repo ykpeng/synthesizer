437b25aa4095ee4cde43eeb4c0913234aed74587
Yi-Ke Peng <yi.ke.peng@gmail.com>
2016-09-26 12:13:59 -0700

    complete phasse 1: frontend structure

---
+++ frontend/synthesizer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Root from './components/root';
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<div>Hi</div>, document.getElementById("root"));
+})
